a0c87697a8d26b7779886138539cf2e9c6eebdaf
David <davidharingri@gmail.com>
2021-02-20 13:18:48 +0100

    Finished homework. Also made some minor changes to the learningDocumentation.txt

---
+++ Lesson_3/homework/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+// VARIABLES FOR GETTING TIME
+const now = new Date();
+const hoursNow = now.getHours();
+const minutesNow = now.getMinutes();
+const secondsNow = now.getSeconds();
+
+// VARIABLES FOR GETTING DAY
+const dayNow = now.getDay() - 1;
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const nameOfTheDay = days[dayNow];
+
+// VARIABLES FOR GETTING MONTH
+const monthNow = now.getMonth();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const nameOfTheMonth = months[monthNow];
+
+// ROUTE WHICH GIVES TIME
+app.get("/time", (req, res) => {
+    res.send({
+        time: hoursNow + ":" + minutesNow + ":" + secondsNow
+    });
+});
+
+// ROUTE WHICH GIVES DAY
+app.get("/day", (req, res) => {
+    res.send({
+        day: nameOfTheDay
+    });
+});
+
+// ROUTE WHICH GIVES MONTH 
+app.get("/month", (req, res) => {
+    res.send({
+        month: nameOfTheMonth
+    });
+});
+
+app.listen(8080);
