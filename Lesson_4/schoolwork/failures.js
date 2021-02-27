0621bfd41c7247880eb389c2a66394220dd1fb15
David <davidharingri@gmail.com>
2021-02-27 09:08:06 +0100

    Completed the learning Documentation for L4. Added some comments in the L4 schoolwork.

--- Lesson_4/schoolwork/failures.js
+++ Lesson_4/schoolwork/failures.js
@@ -1,3 +1,6 @@
+// We aim to use functional approaches when looping
+// like map, filter, reduce, find etc
+
 const failure = ["missclick", "falling down", "skateboard accident", "car accident"
 , "tik tok videos", "stepping in mud"];
 
