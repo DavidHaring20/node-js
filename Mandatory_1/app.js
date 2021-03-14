bd4391b8ea4d0cb681a5b4af00dea6bfd7a18f4e
David <davidharingri@gmail.com>
2021-03-14 16:20:48 +0100

    Finished styling for Home. Reformated the directory and added statemens so I can use images and css from other folders.

--- Mandatory_1/app.js
+++ Mandatory_1/app.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const app = express();
 app.use(express.static('templates'));
+app.use(express.static('assets'));
+app.use(express.static('styles'));
 
 app.get("/home", (req, res) => {
     res.sendFile(__dirname + "/templates/home.html");
-});
-
-app.get("/pourMeSomeWhiskey/:money", (req, res) => {
-    const money = req.params.money;
-
-    console.log("I am here.");
-    
-    if (money > 5) {
-        res.send({
-            msg: "Here you are lad"
-        });
-    } else {
-        res.send({
-            msg: "Get out"
-        });
-    }
 });
 
 app.listen(8080, (error) => {
