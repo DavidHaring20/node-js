9f4cb1263a24137aa1dcb4758575e2ded3037ada
David <davidharingri@gmail.com>
2021-04-20 12:14:10 +0200

    Finished base for recommendations, education and contact. Figured out how to use CSS.

--- Mandatory_2/app.js
+++ Mandatory_2/app.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const app = express();
 
 // Using methods
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,10 +14,10 @@ const header = fs.readFileSync(__dirname + "/public/header/header.html", { encod
 
 const homeContent = fs.readFileSync(__dirname + "/public/home/index.html", { encoding: "UTF-8" });
 const skillsContent = fs.readFileSync(__dirname + "/public/skills/skills.html", { encoding: "UTF-8" });
-const projectsContent = fs.readFileSync(__dirname + "/public/projects/projects.html", { encoding: "UTF-8" })
-// const recommendationsContent = 
-// const educationContent = 
-// const contactContent = 
+const projectsContent = fs.readFileSync(__dirname + "/public/projects/projects.html", { encoding: "UTF-8" });
+const recommendationsContent = fs.readFileSync(__dirname + "/public/recommendations/recommendations.html", { encoding: "UTF-8" });
+const educationContent = fs.readFileSync(__dirname + "/public/education/education.html", { encoding: "UTF-8" });
+const contactContent = fs.readFileSync(__dirname + "/public/contact/contact.html", { encoding: "UTF-8" });
 
 const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", { encoding: "UTF-8" });
 
@@ -35,15 +35,15 @@ app.get("/projects", (req,res) => {
 });
 
 app.get("/recommendations", (req,res) => {
-    
+    res.send(header + recommendationsContent + footer);
 });
 
 app.get("/education", (req,res) => {
-    
+    res.send(header + educationContent + footer);
 });
 
 app.get("/contact", (req,res) => {
-    
+    res.send(header + contactContent + footer);
 });
 
 // Setting up the server
