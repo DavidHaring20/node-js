9a4d079aaf0d66a815b9c1c42a452f34d4c07379
David <davidharingri@gmail.com>
2021-04-27 20:18:32 +0200

    Figured out how to use arrays from routes.

--- Mandatory_2/public/projects/projects.js
+++ Mandatory_2/public/projects/projects.js
@@ -1,5 +1,5 @@
 // Test if the /api/projects worls 
-fetch("/api/projects").then(res => res.json()).then(res => console.log(res));
+// fetch("/api/projects").then(res => res.json()).then(res => console.log(res));
 
 (async function getProjectsFromApi() {
     try {
@@ -19,10 +19,24 @@ fetch("/api/projects").then(res => res.json()).then(res => console.log(res));
             projectDescription.classList.add('project-description');
             projectDescription.innerText = project.projectDescription;
 
-            const 
+            const projectType = document.createElement('p');
+            projectType.classList.add('project-type');
+            projectType.innerText = project.projectType;
+
+            const projectTechUsed = document.createElement('p');
+            projectTechUsed.classList.add('project-tech-used');
+            
+            let techUsed = "";
+            for (tech in project.projectTechUsed) {
+                techUsed += project.projectTechUsed[tech] + "\n";
+            }
+            projectTechUsed.innerText = techUsed;
+
 
             projectDiv.appendChild(projectTitle);
             projectDiv.appendChild(projectDescription);
+            projectDiv.appendChild(projectType);
+            projectDiv.appendChild(projectTechUsed);
             projectsDiv.appendChild(projectDiv);
         });
 
